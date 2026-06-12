68515fe503a40891477c725a9847662ded6ce5dd
Bela <isabelaquintilhano3@gmail.com>
2026-06-12 19:54:31 -0300

    Adicionando o model de pedido

--- src/model/Pedido.java
+++ src/model/Pedido.java
@@ -1,37 +1,29 @@
-package Model;
-
+package model;
 
 public class PedidoModel {
-   /*Atributos privados (segurança do sistema)*/
-   private int id;
-   private int mesa;
+    /*Atributos privados (segurança do sistema)*/
+    private int id;
+    private Mesa mesa;
 
+    /*Construtor: todo pedido nasce com um número d eID e uam Mesa*/
+    public PedidoModel(int id, Mesa mesa) {
+        this.id = id;
+        this.mesa = mesa;
+    }
 
-   /*Construtor: todo pedido nasce com um número d eID e uam Mesa*/
+    public int getId() {
+        return id;
+    }
 
+    public void setId(int id) {
+        this.id = id;
+    }
 
-   public PedidoModel(int id, int mesa) {
-       this.id = id;
-       this.mesa = mesa;
-   }
+    public Mesa getMesa() {
+        return mesa;
+    }
 
-
-   public int getId() {
-       return id;
-   }
-
-
-   public void setId(int id) {
-       this.id = id;
-   }
-
-
-   public int getMesa() {
-       return mesa;
-   }
-
-
-   public void setMesa(int mesa) {
-       this.mesa = mesa;
-   }
+    public void setMesa(Mesa mesa) {
+        this.mesa = mesa;
+    }
 }
