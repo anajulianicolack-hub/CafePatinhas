cdb6fbf7b284bd67bc5c386a6f3ea5bee40243d4
Bela <isabelaquintilhano3@gmail.com>
2026-06-12 09:08:41 -0300

    Adicionando pedido model

--- src/model/Pedido.java
+++ src/model/Pedido.java
@@ -1,4 +1,37 @@
-package model;
+package Model;
 
-public class Pedido {
+
+public class PedidoModel {
+   /*Atributos privados (segurança do sistema)*/
+   private int id;
+   private int mesa;
+
+
+   /*Construtor: todo pedido nasce com um número d eID e uam Mesa*/
+
+
+   public PedidoModel(int id, int mesa) {
+       this.id = id;
+       this.mesa = mesa;
+   }
+
+
+   public int getId() {
+       return id;
+   }
+
+
+   public void setId(int id) {
+       this.id = id;
+   }
+
+
+   public int getMesa() {
+       return mesa;
+   }
+
+
+   public void setMesa(int mesa) {
+       this.mesa = mesa;
+   }
 }
