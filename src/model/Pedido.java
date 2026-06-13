9c83e9f9bdd83d7f52164449dab00e5dfbc748a4
Bela <isabelaquintilhano3@gmail.com>
2026-06-13 11:21:11 -0300

    Reajustando pedido model

--- src/model/Pedido.java
+++ src/model/Pedido.java
@@ -1,14 +1,17 @@
 package model;
 
-public class Pedido {
+public class PedidoModel {
     /*Atributos privados (segurança do sistema)*/
     private int id;
     private Mesa mesa;
+    private Comida comida;
 
     /*Construtor: todo pedido nasce com um número d eID e uam Mesa*/
-    public Pedido(int id, Mesa mesa) {
+
+    public PedidoModel(int id, Mesa mesa, Comida comida) {
         this.id = id;
         this.mesa = mesa;
+        this.comida = comida;
     }
 
     public int getId() {
@@ -25,5 +28,13 @@ public class Pedido {
 
     public void setMesa(Mesa mesa) {
         this.mesa = mesa;
+    }
+
+    public Comida getComida() {
+        return comida;
+    }
+
+    public void setComida(Comida comida) {
+        this.comida = comida;
     }
 }
