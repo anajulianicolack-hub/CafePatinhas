8caffaa86a7f2849e35dbb69400e8c4f4c942b8f
Bela <isabelaquintilhano3@gmail.com>
2026-06-13 11:32:47 -0300

    Reajustando pedido view

--- src/view/PedidoView.java
+++ src/view/PedidoView.java
@@ -1,53 +1,25 @@
 package view;
 
-import model.Comida;
-import model.Mesa;
-import model.Pedido;
+import java.util.Scanner;
 
 public class PedidoView {
     /*Classe responsável por todas as interações visuais no console*/
     /*Relacionado ao fluxo de pedidos, comidas e mesas*/
+    private final Scanner scanner = new Scanner(System.in);
+    public int exibirMenu() {
+        System.out.println("\n===== CAFÉ PATINHAS =====");
+        System.out.println("1 - Cadastrar Comida");
+        System.out.println("2 - Cadastrar Mesa");
+        System.out.println("3 - Cadastrar Pedido");
+        System.out.println("4 - Listar Pedidos");
+        System.out.println("5 - Sair");
 
-    /*Novo, função de exibir os dados detalhados de uma comida especifíca*/
-    public void exibirDadosComida(Comida comida) {
-        System.out.println("===================");
-        System.out.println(" 🐾CAFÉ PATINHAS🐾 ");
-        System.out.println("===================");
-        System.out.println("🧁 Seu pedido de " + comida.getNome() + " foi recebido com sucesso!!");
-        System.out.println("💵 Preço: R$" + comida.getPreco());
-        System.out.println("===================");
-        System.out.println("Detalhes: " + comida.getIngredientes());
-        System.out.println("Contém glúten: " + (comida.isContemGluten() ? "⚠️ Contém glutén" : "🍃 Livre de glutén"));
+        System.out.print("Escolha: ");
+        return Integer.parseInt(scanner.nextLine());
     }
 
-    /*Exibe o status atual de uma mesa*/
-    public void exibirDadosMesa(Mesa mesa) {
-        System.out.println("\n 🪑 [STATUS DA MESA]");
-        System.out.println(" - Número da mesa: " + mesa.getNumero());
-        System.out.println(" - Status: " + (mesa.isOcupada() ? "❌ Ocupada" : "✔️ Livre"));
-    }
-
-    public void exibirPedidoIniciado(int idPedido, int numeroMesa) {
-        System.out.println("===============================================");
-        System.out.println("🐾 [Café Patinhas] Comanda eletrônica iniciada!");
-        System.out.println("📝 Pedido Nº: " + idPedido + " | Mesa: " + numeroMesa);
-        System.out.println("===============================================");
-    }
-
-    public void exibirItemAdicionado(String nomeProduto) {
-        System.out.println(nomeProduto + " foi lançado na comanda da mesa");
-    }
-
-    /*Mostra o resumo do pedido com a comida e a mesa e o valor*/
-    public void exibirResumoPedido(Pedido pedido, Comida comida, double valorTotal) {
-        System.out.println("======================");
-        System.out.println("  RESUMO DO PEDIDO   ");
-        System.out.println("======================");
-        System.out.println("📝 Pedido ID: " + pedido.getId());
-        System.out.println("🪑 Mesa do cliente: " + pedido.getMesa().getNumero());
-        System.out.println("🧁 Item Consumido: " + comida.getNome() + "R$ " + comida.getPreco());
-        System.out.println("💵 Total do pedido: R$ " + valorTotal);
-        System.out.println("======================");
+    public void mostrarMensagem(String msg) {
+        System.out.println(msg);
     }
 }
 
