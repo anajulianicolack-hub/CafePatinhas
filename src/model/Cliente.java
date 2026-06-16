6454d6ede4c06c21b0a45f6688a1f4c36d99bddc
Stephany Ramina <stephanyraminaramina@gmail.com>
2026-06-16 15:16:51 -0300

    Update Cliente.java

--- src/model/Cliente.java
+++ src/model/Cliente.java
@@ -1,16 +1,12 @@
 package model;
 
 public class Cliente extends Pessoa{
-    /*Quantidade de pontos acumulados pelo cliente.*/
     private int pontosFidelidade;
 
-    /*Construtor utilizado para criar um cliente com seus dados.*/
     public Cliente(int id, String nome, String telefone, int pontosFidelidade) {
         super(id, nome, telefone);
         this.pontosFidelidade = pontosFidelidade;
     }
-
-    /*Get e Set dos pontos de fidelidade.*/
     public int getPontosFidelidade() {
         return pontosFidelidade;
     }
@@ -18,8 +14,6 @@ public class Cliente extends Pessoa{
     public void setPontosFidelidade(int pontosFidelidade) {
         this.pontosFidelidade = pontosFidelidade;
     }
-
-    /*Implementação do método abstrato da classe Pessoa.*/
     public void exibirInformacoes() {
         System.out.println("Cliente: " + nome);
         System.out.println("Telefone: " + telefone);
