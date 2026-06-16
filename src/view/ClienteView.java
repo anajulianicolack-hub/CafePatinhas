53a1d51d30783401d9b49720b890abf3af1f9c86
Stephany Ramina <stephanyraminaramina@gmail.com>
2026-06-16 15:18:19 -0300

    Update ClienteView.java

--- src/view/ClienteView.java
+++ src/view/ClienteView.java
@@ -1,13 +1,9 @@
 package view;
 
 import java.util.Scanner;
-
-/*Classe responsável pela interação com o usuário.*/
 public class ClienteView {
-    /*Scanner utilizado para ler os dados digitados.*/
     private final Scanner scanner = new Scanner(System.in);
 
-    /*Métodos para capturar os dados do cliente.*/
     public int lerId() {
         System.out.print("ID: ");
         return Integer.parseInt(scanner.nextLine());
@@ -27,8 +23,7 @@ public class ClienteView {
         System.out.print("Pontos de Fidelidade: ");
         return Integer.parseInt(scanner.nextLine());
     }
-
-    /*Exibe o menu de opções do CRUD.*/
+    
     public int exibirMenu() {
 
         System.out.println("\n===== CLIENTES =====");
@@ -43,7 +38,6 @@ public class ClienteView {
         return Integer.parseInt(scanner.nextLine());
     }
 
-    /*Exibe mensagens para o usuário.*/
     public void mostrarMensagem(String msg) {
         System.out.println(msg);
     }
