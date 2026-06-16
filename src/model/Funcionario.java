4f397f2521c9d9379be54e284bdf72d406f15745
Stephany Ramina <stephanyraminaramina@gmail.com>
2026-06-16 15:17:26 -0300

    Update Funcionario.java

--- src/model/Funcionario.java
+++ src/model/Funcionario.java
@@ -1,22 +1,15 @@
 package model;
 
 public class Funcionario extends Pessoa{
-
-    /*Cargo ocupado pelo funcionário.*/
     private final String cargo;
 
-    /*Construtor utilizado para criar um funcionário com seus dados.*/
     public Funcionario(int id, String nome, String telefone, String cargo) {
         super(id, nome, telefone);
         this.cargo = cargo;
     }
-
-    /*Get para acessar o cargo.*/
     public String getCargo() {
         return cargo;
     }
-
-    /*Implementação do método abstrato da classe Pessoa.*/
     public void exibirInformacoes() {
         System.out.println("Funcionário: " + nome);
         System.out.println("Cargo: " + cargo);
